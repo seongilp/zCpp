ea5111ee3ed24f491aef7692c37c328e6e2fc80e
root <root@arch.(none)>
2008-11-24 21:56:15 +0200

    I completed my homeworkls

--- ex7/xmltemperature_pohja.cpp
+++ ex7/xmltemperature_pohja.cpp
@@ -2,8 +2,30 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+string find_page(string src, string text) {
+  //  cout << "I want to find.." << text << endl;
+  //cout << "its length is.." << text.length() << endl;
+  string stag, etag, result;
+  size_t found1, found2, found3;
+  stag =  "<" + text + ">";
+  etag = "</" + text + ">";
+  found1 = src.find(stag);
+  found2 = src.find(etag);
+  //cout << "found position is .." << found1 << endl;
+  found1 = found1 + stag.length();
+  found3 = found2 - found1;
+  if(found1 != string::npos) {
+    result = src.substr(found1,found3); 
+    return result;
+  }
+  else {
+    return "No string here..";
+  }
+}
 
 
 int main (void) {
@@ -21,8 +43,10 @@ int main (void) {
 	// extracts location and temperature from the XML
 	// data in the string page and stores them in 
 	// the strings location and temperature respectively
+
+	location    = find_page(page,"location");
+	temperature = find_page(page,"temp_c");
 	
 	cout << "Location: " << location << endl;
 	cout << "Temperature: " << temperature << endl; 
-	system("pause");
 }
